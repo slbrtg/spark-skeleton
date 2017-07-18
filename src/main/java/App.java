2710ffacb17411ee6b573c98fc647541bcf7869f
slbrtg <slbrtg@gmail.com>
2017-07-18 09:39:07 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,14 @@ import static spark.Spark.*;
 public class App {
 
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      model.put("template", "templates/index.vtl");
+      return new VelocityTemplateEngine().render(
+        new ModelAndView(model, layout)
+      );
+    });
   }
 }
