2543df5e52f4236fe2ee2135062a58682994c7b4
slbrtg <slbrtg@gmail.com>
2017-07-24 15:53:11 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,7 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new VelocityTemplateEngine().render(
         new ModelAndView(model, layout)
